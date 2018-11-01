487e2b8043d5167b05c55bfa6fe9ef06d80330bc
StephanieHuang0801 <1029476703@qq.com>
2018-11-01 20:13:33 +0800

    节点 事件

--- Users/10294/Documents/用例/js基础01/common.js
+++ Users/10294/Documents/用例/js基础01/common.js
@@ -1,3 +1,10 @@
 function my$(id){
     return document.getElementById(id);
 }
+function setInnerText(element,text){
+    if(typeof element.textContent!="undefined"){
+        element.textContent=text;
+    }else {
+        element.innerText=text;
+    }
+}
